17c03da18be2bf903d8ac460b847c24d7c48ef04
igbregon <ig.bregon@gmail.com>
2025-05-30 21:45:27 -0400

    fix 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://igbregon.github.io',
+  base: 'bigdata_landpage',
 });
